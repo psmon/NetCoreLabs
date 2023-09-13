169dddf4a452a7d14760a1ca4dd2b89d91df5f71
psmon <psmon@live.co.kr>
2023-09-13 11:33:15 +0900

    Blazor Assem 전환

--- BlazorActorApp/Shared/WeatherForecast.cs
+++ BlazorActorApp/Shared/WeatherForecast.cs
@@ -1,4 +1,4 @@
-namespace BlazorApp.Data
+namespace BlazorActorApp.Shared
 {
     public class WeatherForecast
     {
@@ -6,8 +6,8 @@ namespace BlazorApp.Data
 
         public int TemperatureC { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
         public string? Summary { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
     }
 }
