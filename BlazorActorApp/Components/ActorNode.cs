f2da129514fec72d7036478618f2cd5bfe77d214
psmon <psmon@live.co.kr>
2023-09-14 16:23:08 +0900

    BlazorActorAPP 추가

---
+++ BlazorActorApp/Components/ActorNode.cs
@@ -0,0 +1,13 @@
+﻿using Blazor.Diagrams.Core.Geometry;
+using Blazor.Diagrams.Core.Models;
+
+namespace BlazorActorApp.Components
+{
+    public class ActorNode : NodeModel
+    {
+        public ActorNode(Point? position = null) : base(position) { }
+
+        public double MessageCount { get; set; }
+
+    }
+}
