e1690f42c7ab49a622f0c648e8e142059c8cb409
psmon <psmon@live.co.kr>
2023-09-16 09:14:56 +0900

    [Blazor] BroadCast/Random Router 추가

--- BlazorActorApp/Program.cs
+++ BlazorActorApp/Program.cs
@@ -25,13 +25,23 @@ var akkaService = app.Services.GetRequiredService<AkkaService>();
 // Create ActorSystem
 var actorSystem = akkaService.CreateActorSystem("BlazorActorSystem");
 
-// Create MonitorActor
-var simpleMonitorActor = actorSystem.ActorOf(Props.Create<SimpleMonitorActor>());
-akkaService.AddActor("simpleMonitorActor", simpleMonitorActor);
-
-// Create RoundRobin
+// Create RoundRobin Router
 var roundrobin = actorSystem.ActorOf(Props.Create<BasicActor>().WithRouter(new RoundRobinPool(0)), "roundrobin");
 akkaService.AddActor("roundrobin", roundrobin);
+var roundrobinMonitor = actorSystem.ActorOf(Props.Create<SimpleMonitorActor>());
+akkaService.AddActor("roundrobinMonitor", roundrobinMonitor);
+
+// Create BroadCast Router
+var broadcast = actorSystem.ActorOf(Props.Create<BasicActor>().WithRouter(new BroadcastPool(0)), "broadcast");
+akkaService.AddActor("broadcast", broadcast);
+var broadcastMonitor = actorSystem.ActorOf(Props.Create<SimpleMonitorActor>());
+akkaService.AddActor("broadcastMonitor", broadcastMonitor);
+
+// Random Router
+var random = actorSystem.ActorOf(Props.Create<BasicActor>().WithRouter(new RandomPool(0)), "random");
+akkaService.AddActor("random", random);
+var randomMonitor = actorSystem.ActorOf(Props.Create<SimpleMonitorActor>());
+akkaService.AddActor("randomMonitor", randomMonitor);
 
 
 // Configure the HTTP request pipeline.
