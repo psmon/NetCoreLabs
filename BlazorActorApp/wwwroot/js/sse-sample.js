b2d20aafb21fafb08d3a64c0b61aabad7fbd9ef5
psmon <psmon@live.co.kr>
2024-05-12 16:48:06 +0900

    sse 브로드캐스트 추가

--- BlazorActorApp/wwwroot/js/sse-sample.js
+++ BlazorActorApp/wwwroot/js/sse-sample.js
@@ -1,18 +1,31 @@
-﻿function display(id) {
+﻿function StartSSE(id) {
     var source = new EventSource('/api/sse/message/' + id);
+    var source2 = new EventSource('/api/sse/message/all');
 
-    var ul = document.getElementById("sse");
-    source.onmessage = function (e) {
-        var li = document.createElement("li");
+    var ul = document.getElementById('sse');
+    var ul2 = document.getElementById('sse2');
+
+    source.onmessage = function (e) {      
+        var li = document.createElement('li');
         if (!!e.data) {
             var retrievedData = JSON.parse(e.data)
             li.textContent = retrievedData.Message;
             ul.appendChild(li);
-
             console.log(retrievedData.Message);
-
-        }
-        
+        }        
     }
+
+    source2.onmessage = function (e) {
+        var li = document.createElement('li');
+        if (!!e.data) {
+            var retrievedData = JSON.parse(e.data)
+            li.textContent = retrievedData.Message;
+            ul2.appendChild(li);
+            console.log(retrievedData.Message);
+        }
+    }
+
+
+
 }
 
