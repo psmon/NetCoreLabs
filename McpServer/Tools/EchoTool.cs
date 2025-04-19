970f86de2a07e6b6270c229b6c021f907ff0b942
psmon <psmon@live.co.kr>
2025-04-19 10:28:45 +0900

    MCP Server 추가

---
+++ McpServer/Tools/EchoTool.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using ModelContextProtocol.Server;
+
+namespace McpServer.Tools;
+
+[McpServerToolType]
+public static class EchoTool
+{
+    [McpServerTool, Description("Echoes the message back to the client.")]
+    public static string Echo(string message) => $"Hello from C#: {message}";
+
+    [McpServerTool, Description("Echoes in reverse the message sent by the client.")]
+    public static string ReverseEcho(string message) => new string(message.Reverse().ToArray());
+}
